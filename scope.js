b7abbe3c7379bdb0d6eea5830706aa5480c3cc5f
Shakil Ahmed <shakil.devcraft@gmail.com>
2025-03-16 13:00:34 +0600

    var is gobal scope variable and const, let block scope variable

---
+++ scope.js
@@ -0,0 +1,16 @@
+function add(a, b){
+    const total = a + b;
+    // console.log(a, b);
+    if(b > 5){
+        const sum = 25 + a + b;
+    }else{
+        const sum = 6 + a + b;
+        var current = sum;
+    }
+    console.log(current);
+    return total;
+}
+// console.log(sum);
+
+// console.log(total);
+add(6, 2);
